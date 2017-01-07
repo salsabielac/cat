b875c16b7b3dc32c57031d5a0be18371ea527d72
Salsabiela Cahyahaq <salsabielacq@gmail.com>
2017-01-07 18:57:34 +0700

    Add files via upload
    user dobel, lek password salah gak ngaruh

--- loginp.php
+++ loginp.php
@@ -3,16 +3,13 @@ session_start();
 include 'connect.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
-$login = mysql_query("SELECT * FROM cat where username='$username' and password='$password'");
+$login = mysql_query("SELECT * FROM user where username='$username' and password='$password'");
 if(mysql_num_rows($login)==1){
 	$row=$login->fetch_assoc();
-	$_SESSION['role'] = $row['role'];
-	if ($row['role']=='cat_files'){
 		$_SESSION['username']=$username;
-		header('location:cat_files/home.php');
+		header('location:index.php');
 	}else{
 		$_SESSION['username']=$username;
-		header('location:home.php');	
+		header('location:index-before.php');	
 	}
-}
 ?>
